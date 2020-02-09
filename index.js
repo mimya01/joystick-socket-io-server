5bb591f10e20222ff679b0086ceddbf1c674dc8c
mouhcine mimya <mouhcine.mimya@gmail.com>
2020-02-10 00:46:50 +0100

    stable release

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const server = require('http').createServer()
 const io = require('socket.io')(server)
-const port = 3000
+const port = 4000
 io.on('connection', client => {
     console.log('new client connected')
-    client.on('event', data => {
+    client.on('direction', data => {
         console.log(data)
+        client.broadcast.emit(data.Degree)
     });
+    /* client.on('event', data => {
+        console.log(data)
+    }); */
     client.on('disconnect', () => {
         console.log('client connected')
     })
 })
 server.listen(port, () =>
-    console.log('listening on :3000')
+    console.log('listening on :4000')
 );
